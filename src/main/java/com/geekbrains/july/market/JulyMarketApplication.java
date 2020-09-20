dc8e4e93a5bdb8afd3ce7be2458cc54f07e4f460
Pavel.Novoselov <Pavel.Novoselov@excelian.com>
2020-09-20 15:12:24 +0300

    first commit

---
+++ src/main/java/com/geekbrains/july/market/JulyMarketApplication.java
@@ -0,0 +1,19 @@
+package com.geekbrains.july.market;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JulyMarketApplication {
+	// Домашнее задание:
+	// 1. Добавить модульные (юнит) и интеграционные тесты (по своим возможностям, минимум 5 тестов)
+
+	// Что сделаем на 8-м занятии:
+	// 1. Добавим всю цепочку оформления заказа
+	// 2. Добавим фильтры
+	// 3. Добавим регистрацию пользователей
+
+	public static void main(String[] args) {
+		SpringApplication.run(JulyMarketApplication.class, args);
+	}
+}
